4f617d17a33003f80c6cb68d5de8a68cfadea1a1
AlexLinux <alejugger@gmail.com>
2024-06-15 23:21:48 -0600

    more grid

--- examples/src/pages/Page3.jsx
+++ examples/src/pages/Page3.jsx
@@ -7,14 +7,16 @@ const Page3 = () => {
             <h1>Page 3</h1>
             <p>Welcome to Page 3!</p>
             <p>This is an example of several grid elemnts</p>
-            <hr/>
-            <p>Grid 3x3 using</p>
-            <p>grid-template-columns: repeat(3, 100px);</p>
-            <p>grid-template-rows: repeat(3, 100px);</p>
-            <p>the same as using grid-template-columns: 100px 100px 100px;</p>
+            <hr />
+            <div className="grid-instructions">
+                <p>Grid 3x3 using</p>
+                <p>grid-template-columns: repeat(3, 100px);</p>
+                <p>grid-template-rows: repeat(3, 100px);</p>
+                <p>the same as using grid-template-columns: 100px 100px 100px;</p>
+            </div>
             <div className="grid-container">
-                
-                
+
+
                 <div className="grid-item">1</div>
                 <div className="grid-item">2</div>
                 <div className="grid-item">3</div>
@@ -25,6 +27,18 @@ const Page3 = () => {
                 <div className="grid-item">8</div>
                 <div className="grid-item">9</div>
             </div>
+            <div className="grid-instructions">
+                <p>Grid 5x5 using house map</p>
+
+            </div>
+            <div className="container-house">
+                <div className="room" id="living-room">Living Room</div>
+                <div className="room" id="kitchen">Kitchen</div>
+                <div className="room" id="bedroom">Bedroom</div>
+                <div className="room" id="bathroom">Bathroom</div>
+                <div className="room" id="closet">Closet</div>
+            </div>
+            <hr className='divider'/>
         </div>
     );
 };
